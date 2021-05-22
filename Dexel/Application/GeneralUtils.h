557f5b958db82b446b97562e81a1b8258545c1f5
Delcho <del_dichev@abv.bg>
2021-05-22 22:08:00 +0300

    SPREAD-24 Update GeneralUtils.h by adding new function for reading row when editing

--- Dexel/Application/GeneralUtils.h
+++ Dexel/Application/GeneralUtils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "CellUtils.h"
 char converToLower(char symbol)
 {
     if (symbol <= 'Z' && symbol >= 'A')
@@ -18,4 +19,29 @@ std::string lowerred(std::string stringToLower)
         *i = converToLower(*i);
     }
     return stringToLower;
+}
+bool readForEdit(const std::string str, char endSym, int& intHolder, int* lengthRead)
+{
+	int index = 0;
+	for (index; str[index] == ' '; ++index);
+	bool sign = true;
+	if (str[index] == '-') {
+		++index;
+		sign = false;
+	}
+	int number = 0;
+	for (index; str[index] != endSym && str[index] != '\n' && str[index] != '\0'; index++) {
+		if (isDigit(str[index])) {
+			number *= 10;
+			number += str[index] - '0';
+		}
+		else {
+			return false;
+		}
+	}
+	intHolder = number;
+	if (lengthRead != NULL) {
+		*lengthRead = index;
+	}
+	return true;
 }
