2360e3a89a8eee4acd84e195ad9edd724fa4e3a8
Delcho <del_dichev@abv.bg>
2021-05-25 21:51:44 +0300

    SPREAD-16 Cell.h update

--- Dexel/Application/Cell.h
+++ Dexel/Application/Cell.h
@@ -9,8 +9,9 @@ public:
 	Cell(std::string _cellContent);
 	Cell(const Cell& rhs);
 	Cell& operator=(Cell& rhs);
-
-	virtual void print(int width) const;
+	virtual void setContent(const std::string cellNewContent) = 0;
+	virtual void writeToFile(std::fstream& writer)=0;
+	virtual void print(int width)const;
 	virtual Cell* copyCell() = 0;
 };
 
