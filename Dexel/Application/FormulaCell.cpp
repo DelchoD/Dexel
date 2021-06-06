502bfe930841493fb6d18a31cdb4c9df9726a03e
Delcho <del_dichev@abv.bg>
2021-06-06 23:09:54 +0300

    Added information about the cells involved in the formula and variable for accessing them. Added function for getting double

--- Dexel/Application/FormulaCell.cpp
+++ Dexel/Application/FormulaCell.cpp
@@ -1,19 +1,84 @@
 #include "FormulaCell.h"
+#include "GeneralUtils.h"
+#include <cmath>
 #include <iostream>
-FormulaCell::FormulaCell(const char* _cellContent): Cell(_cellContent)
+#include <fstream>
+FormulaCell::FormulaCell(const char* _cellContent, const Table* transfer) : Cell(_cellContent), tabletranfer(transfer)
 {
 	char* reader = cellContent;
 	for (reader; *reader != ' '; ++reader);
 	*reader = '\0';
+	int end = 0;
+	for (; !IsTableRowIndex (*reader); ++reader);
+	getCell(reader, end, rowFirstCell, columnFirstCell);
+	reader += end;
+
+	for (; !isOperator(*reader); reader++);
+	sign = *reader;
+
+	for (++reader; !IsTableRowIndex(*reader); ++reader);
+	getCell(reader, end, rowSecondCell, columnSecondCell);
 }
 
-//void FormulaCell::writeToFile(std::fstream& writer)
-//{
-//}
-//
-//void FormulaCell::print()
-//{
-//}
+double FormulaCell::examine()
+{
+	double operand1 = tabletranfer->getCell(rowFirstCell - 1, columnFirstCell - 1)->examine();
+	double operand2 = tabletranfer->getCell(rowSecondCell - 1, columnSecondCell - 1)->examine();
+	switch (sign) 
+	{
+	case '+':
+		return operand1 + operand2;
+		break;
+	case '-':
+		return operand1 - operand2;
+		break;
+	case '*':
+		return operand1 * operand2;
+		break;
+	case '/':
+		return operand1 / operand2;
+	case '^':
+		return pow(operand1, operand2);
+		break;
+	}
+}
+
+void FormulaCell::getCell(const char* string, int& end, int& rowIndex, int& columnIndex)
+{
+	int row = 0, col = 0;
+	int index = 1;
+	for (; string[index] != 'C' && string[index] != '\0'; index++) 
+	{
+		row *= 10;
+		row += string[index] - '0';
+	}
+	for (++index; isDigit(string[index]) && string[index] != '\0'; index++) 
+	{
+		col *= 10;
+		col += string[index] - '0';
+	}
+	rowIndex = row;
+	columnIndex = col;
+	end = index;
+}
+
+void FormulaCell::writeToFile(std::fstream& writer)
+{
+	double checkForNULL = tabletranfer->getCell(rowSecondCell - 1, columnSecondCell - 1)->examine();
+	if (sign == '/' && checkForNULL == 0)
+	{
+		writer << "ERROR";
+	}
+	else 
+	{
+		writer << examine();
+	}
+}
+
+void FormulaCell::print()
+{
+	std::cout << examine();
+}
 
 Cell* FormulaCell::copyCell()
 {
--- Dexel/Application/FormulaCell.h
+++ Dexel/Application/FormulaCell.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "Cell.h"
+#include "Table.h"
 class FormulaCell : public Cell
 {
 private:
+	Cell* cellsToUse;
+	const Table* tabletranfer;
+	int rowFirstCell, columnFirstCell;
+	int rowSecondCell, columnSecondCell;
+	char sign;
 public:
-	FormulaCell(const char* _cellContent);
-	/*virtual void writeToFile(std::fstream& writer);
-	virtual void print();*/
+	FormulaCell(const char* _cellContent, const Table* transfer);
+	virtual double examine();
+	void getCell(const char* string, int& end, int& rowIndex, int& colIndex);
+	virtual void writeToFile(std::fstream& writer);
+	virtual void print();
 	virtual Cell* copyCell();
 };
 
