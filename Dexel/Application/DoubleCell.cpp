bc02a06c3c3806519deed96fe264c53b40c50cc9
Delcho <del_dichev@abv.bg>
2021-06-06 21:29:06 +0300

    SPREAD-13 Added double interpretation of a cell

--- Dexel/Application/DoubleCell.cpp
+++ Dexel/Application/DoubleCell.cpp
@@ -5,6 +5,28 @@ DoubleCell::DoubleCell(const char* _cellContent):Cell(_cellContent)
 	size_t index = 0;
 	for (cellContent; cellContent[index] != ' '&& cellContent[index]!='\0'; ++index);
 	*cellContent = '\0';
+	bool hasSign = cellContent[0] != '-';
+	double number = 0;
+	int exp = 1;
+	bool afterDecimalPoint = false;
+	for (index = (cellContent[0] == '-') ? 1 : 0; cellContent[index] != '\0'; index++) 
+	{
+		if (cellContent[index] == '.') 
+		{
+			afterDecimalPoint = true;
+		}
+		else 
+		{
+			number *= 10;
+			number += cellContent[index] - '0';
+			if (afterDecimalPoint) 
+			{
+				exp *= 10;
+			}
+		}
+	}
+
+	cellValueAsDouble = hasSign ? number / exp : -(number / exp);
 }
 
 //void DoubleCell::writeToFile(std::fstream& writer)
