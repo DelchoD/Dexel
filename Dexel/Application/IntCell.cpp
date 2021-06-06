66431df1412862a85b90cb01bb6e072ab6ae4944
Delcho <del_dichev@abv.bg>
2021-06-06 21:29:45 +0300

    SPREAD-12 Added int interpretation of a cell

--- Dexel/Application/IntCell.cpp
+++ Dexel/Application/IntCell.cpp
@@ -5,6 +5,16 @@ IntCell::IntCell(const char* _cellContent):Cell(_cellContent)
 	size_t index = 0;
 	for (cellContent; cellContent[index] != ' ' && cellContent[index] != '\0'; ++cellContent);
 	*cellContent = '\0';
+	bool hasSign = cellContent[0] != '-';
+	int number = 0;
+	for (index = (cellContent[0] == '-') ? 1 : 0; cellContent[index] != '\0'; index++) 
+	{
+		number *= 10;
+		number += cellContent[index] - '0';
+	}
+
+	cellValueAsInt = hasSign ? number : -number;
+
 }
 
 //void IntCell::writeToFile(std::fstream& writer)
